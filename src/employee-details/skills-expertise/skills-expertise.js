b46a8eb3470ff11096f46023dedd1d024b6bdae8
Chathuranga Tennakoon <chathuranga@cyrup.sg>
2018-09-05 16:43:47 +0530

    modal added

--- src/employee-details/skills-expertise/skills-expertise.js
+++ src/employee-details/skills-expertise/skills-expertise.js
@@ -44,11 +44,112 @@ export class SkillsExpertise extends Component {
                 <div className="row">
                     <div className="form-group text-center m-b-0">
                         <button className="btn btn-primary waves-effect waves-light"
-                                data-toggle="modal" data-target="#signup-modal"
+                                data-toggle="modal" data-target="#myModal"
                                 type="button">
                             <i className="mdi mdi-plus"></i>
                             Add New Category
                         </button>
+                    </div>
+                </div>
+
+
+                <div className="row">
+                    <div className="modal" id="myModal">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+
+                                <div className="modal-header">
+                                    <h4 className="modal-title">Modal Heading</h4>
+                                    <button type="button" className="close" data-dismiss="modal">&times;</button>
+                                </div>
+
+                                <div className="modal-body">
+                                    Modal Body ...
+                                </div>
+
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div className="row">
+                    <div className="col-12">
+                        <div className="card-box">
+                            <div id="signup-modal" className="modal fade" tabindex="-1" role="dialog"
+                                 aria-labelledby="custom-width-modalLabel" aria-hidden="true">
+                                <div className="modal-dialog">
+                                    <div className="modal-content">
+                                        <div className="modal-body">
+                                            <h2 className="text-uppercase text-center m-b-30">
+                                                <a href="index.html" className="text-success">
+                                                    <span><img src="assets/images/logo.png" alt="" height="28"/></span>
+                                                </a>
+                                            </h2>
+
+
+                                            <form className="form-horizontal" action="#">
+
+                                                <div className="form-group m-b-25">
+                                                    <div className="col-12">
+                                                        <label for="username">Name</label>
+                                                        <input className="form-control" type="email"
+                                                               id="username" required=""
+                                                               placeholder="Michael Zenaty"/>
+                                                    </div>
+                                                </div>
+
+                                                <div className="form-group m-b-25">
+                                                    <div className="col-12">
+                                                        <label for="emailaddress">Email
+                                                            address</label>
+                                                        <input className="form-control" type="email"
+                                                               id="emailaddress" required=""
+                                                               placeholder="john@deo.com"/>
+                                                    </div>
+                                                </div>
+
+                                                <div className="form-group m-b-25">
+                                                    <div className="col-12">
+                                                        <label for="password">Password</label>
+                                                        <input className="form-control" type="password"
+                                                               required="" id="password"
+                                                               placeholder="Enter your password"/>
+                                                    </div>
+                                                </div>
+
+                                                <div className="form-group m-b-20">
+                                                    <div className="col-12">
+                                                        <div className="checkbox checkbox-custom">
+                                                            <input id="checkbox11" type="checkbox"
+                                                                   checked/>
+                                                            <label for="checkbox11">
+                                                                I accept <a href="#">Terms and
+                                                                Conditions</a>
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="form-group account-btn text-center m-t-10">
+                                                    <div className="col-12">
+                                                        <button
+                                                            className="btn w-lg btn-rounded btn-primary waves-effect waves-light"
+                                                            type="submit">Sign Up Free
+                                                        </button>
+                                                    </div>
+                                                </div>
+
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
